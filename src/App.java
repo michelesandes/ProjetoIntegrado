8204827f0e8b6d350746a51f207116835ee453f6
Michele Sandes Feitosa <chelesanfeitosa@gmail.com>
2023-05-25 18:10:26 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Conversor de Decimal para Binário");
 
-        
+        int decimalNumero;
+        int contagem;
+        int Resultado;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println(teclado + "Número digitado");
+
 
 
     }
