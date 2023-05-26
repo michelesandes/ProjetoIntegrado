3db76f2774f7515bae29acbeea030651ad8b79b3
Michele Sandes Feitosa <chelesanfeitosa@gmail.com>
2023-05-25 21:38:43 -0300

    Update App.java
    atualizando

--- src/App.java
+++ src/App.java
@@ -6,12 +6,25 @@ public class App {
 
         int decimalNumero;
         int contagem;
-        int Resultado;
+        int resultado;
+        int resto;
 
+        System.out.print("Digite um valor entre 1 e 100: ");
         Scanner teclado = new Scanner(System.in);
         decimalNumero = teclado.nextInt();
-        if(decimalNumero == 5){
-            System.out.println (decimalNumero + "Número digitado");
+        if(decimalNumero >=1 && decimalNumero<=100){
+            contagem = 0;
+            System.out.println (" -- número digitado --- " + decimalNumero);
+            while (decimalNumero != 1) {
+                resultado = decimalNumero/2;
+                resto = decimalNumero%2;
+                int binarioNum[] = new int[12];
+                binarioNum[contagem] = resto;
+                decimalNumero = resultado;
+                System.out.print("Resultado = "+resultado+" .. resto = " +resto+" ..contagem = " +contagem);
+                contagem +=1;
+                
+            }
 
         }else{
             System.out.println("tente de novo");
